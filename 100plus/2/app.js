d5b5db3038d2e65493a8388ef1d4956c0975546c
kroobeet <kroobeet@gmail.com>
2020-09-08 17:15:46 +0600

    update files

--- 100plus/2/app.js
+++ 100plus/2/app.js
@@ -1,14 +1,16 @@
-var arrayHEXElements = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+var arrayHEXElements = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 
-function clickBtn(elem) {
+function clickBtn() {
 	var color = randomColor(arrayHEXElements);
+
 	changeBackground(color);
+
 	document.getElementById('hex').innerHTML = String(color);
 }
 
 function randomColor(arr) {
 	var color = "#";
-	
+
 	for (let i = 1; i <= 6; i++) {
 		var elemArr = getRandomInt(0,15);
 		color += String(arr[elemArr]);
@@ -17,12 +19,10 @@ function randomColor(arr) {
 	return color;
 }
 
-
-
 function getRandomInt(min,max) {
 	return Math.floor(Math.random() * (max-min+1) + min);
 }
 
-function changeBackground (color) {
+function changeBackground(color) {
 	document.body.style.background = color;
 }
