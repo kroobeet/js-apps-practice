ac1c8f72dacd8959d28c749c482376cb90845640
kroobeet <kroobeet@gmail.com>
2020-09-18 17:21:02 +0600

    update

--- 100plus/7/app.js
+++ 100plus/7/app.js
@@ -2,27 +2,27 @@
 var testimonialObj = { 
 	1: {
 		Photo: './img/customer-1.jpg',
-		Name: 'SANDY',
+		Name: 'Sandy',
 		Testimonial: 'Accusantium, consectetur, eum. Voluptates voluptatem tempora corrupti enim harum, qui, tempore possimus non optio fugit laudantium consequuntur aliquid veritatis, eius adipisci suscipit, iusto eligendi dignissimos! Error, numquam eos mollitia autem!'
 	},
 	2: {
 		Photo: './img/customer-2.jpg',
-		Name: 'AMY',
+		Name: 'Amy',
 		Testimonial: 'Consequuntur rem suscipit ut beatae laudantium ab eum rerum vero. Obcaecati ipsam atque quia cumque cum quidem, deleniti enim, illum aliquid ea.'
 	},
 	3: {
 		Photo: './img/customer-3.jpg',
-		Name: 'TYRELL',
+		Name: 'Tyrell',
 		Testimonial: 'Tempore sed, voluptate commodi, dolor quos doloremque eum reiciendis facere accusamus consectetur molestias perspiciatis suscipit velit. Aut consequuntur facilis laborum vitae itaque.'
 	},
 	4: {
 		Photo: './img/customer-4.jpg',
-		Name: 'WANDA',
+		Name: 'Wanda',
 		Testimonial: 'Voluptatem voluptatum error quidem nemo eaque eveniet quasi temporibus, ea quas nostrum quis dolorem, dolor doloribus omnis eos ab repellat fugiat consequuntur?'
 	},
 	5: {
 		Photo: './img/customer-0.jpg',
-		Name: 'JOHN',
+		Name: 'John',
 		Testimonial: 'Dolorem dicta deserunt voluptas earum itaque corrupti accusantium quos aut molestiae reiciendis, sed suscipit impedit saepe laboriosam beatae eveniet rem, commodi, molestias!'
 	}
 };
