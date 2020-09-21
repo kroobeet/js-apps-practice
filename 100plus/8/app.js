f5e90db40637a63894d28ebe2dfa5df1e3a8becd
kroobeet <kroobeet@gmail.com>
2020-09-21 21:35:59 +0600

    update

--- 100plus/8/app.js
+++ 100plus/8/app.js
@@ -1,3 +1,74 @@
+var products = {
+	'Cakes': {
+		1: {
+			Photo: './img/cake-1.jpeg',
+			Name: 'Cake Item',
+			Price: 5
+		},
+		2: {
+			Photo: './img/cake-2.jpeg',
+			Name: 'Cake Item',
+			Price: 10
+		},
+		3: {
+			Photo: './img/cake-3.jpeg',
+			Name: 'Cake Item',
+			Price: 15
+		}
+	},
+	'Cupcakes': {
+		1: {
+			Photo: './img/cupcake-1.jpeg',
+			Name: 'Cupcake Item',
+			Price: 5
+		},
+		2: {
+			Photo: './img/cupcake-1.jpeg',
+			Name: 'Cupcake Item',
+			Price: 5
+		},
+		3: {
+			Photo: './img/cupcake-1.jpeg',
+			Name: 'Cupcake Item',
+			Price: 5
+		}
+	},
+	'Sweet': {
+		1: {
+			Photo: './img/sweets-1.jpeg',
+			Name: 'Sweet Item',
+			Price: 5
+		},
+		2: {
+			Photo: './img/sweets-2.jpeg',
+			Name: 'Sweet Item',
+			Price: 10
+		},
+		3: {
+			Photo: './img/sweets-3.jpeg',
+			Name: 'Sweet Item',
+			Price: 15
+		}
+	},
+	'Doughnuts': {
+		1: {
+			Photo: './img/doughnut-1.jpeg',
+			Name: 'Dougnut Item',
+			Price: 5
+		},
+		2: {
+			Photo: './img/doughnut-2.jpeg',
+			Name: 'Dougnut Item',
+			Price: 10
+		},
+		3: {
+			Photo: './img/doughnut-3.jpeg',
+			Name: 'Dougnut Item',
+			Price: 15
+		}
+	}
+}
+
 var border = document.querySelector('#border');
 function move() {
 	border.style.marginLeft = '-10px';
@@ -8,7 +79,3 @@ function out() {
 	border.style.marginLeft = '-55px';
 	border.style.marginTop = '-55px';
 }
-
-var storeItem = document.querySelectorAll('.store-item').addEventListener('mousemove', (function(event) {
-	
-}));
