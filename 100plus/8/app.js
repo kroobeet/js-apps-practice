768d156653ea714bd7235842e71343219c4311d3
kroobeet <kroobeet@gmail.com>
2020-09-21 19:39:05 +0600

    update files

---
+++ 100plus/8/app.js
@@ -0,0 +1,14 @@
+var border = document.querySelector('#border');
+function move() {
+	border.style.marginLeft = '-10px';
+	border.style.marginTop = '0px';
+}
+
+function out() {
+	border.style.marginLeft = '-55px';
+	border.style.marginTop = '-55px';
+}
+
+var storeItem = document.querySelectorAll('.store-item').addEventListener('mousemove', (function(event) {
+	
+}));
