f5bfdd7b66e06072b1893b9ae8f3f51329b28fca
kroobeet <kroobeet@gmail.com>
2020-09-22 18:39:17 +0600

    update

--- 100plus/8/app.js
+++ 100plus/8/app.js
@@ -1,106 +1,7 @@
-var products = {
-	'Cakes': {
-		1: {
-			Photo: './img/cake-1.jpeg',
-			Name: 'Cake Item',
-			Price: 5
-		},
-		2: {
-			Photo: './img/cake-2.jpeg',
-			Name: 'Cake Item',
-			Price: 10
-		},
-		3: {
-			Photo: './img/cake-3.jpeg',
-			Name: 'Cake Item',
-			Price: 15
-		}
-	},
-	'Cupcakes': {
-		1: {
-			Photo: './img/cupcake-1.jpeg',
-			Name: 'Cupcake Item',
-			Price: 5
-		},
-		2: {
-			Photo: './img/cupcake-1.jpeg',
-			Name: 'Cupcake Item',
-			Price: 5
-		},
-		3: {
-			Photo: './img/cupcake-1.jpeg',
-			Name: 'Cupcake Item',
-			Price: 5
-		}
-	},
-	'Sweets': {
-		1: {
-			Photo: './img/sweets-1.jpeg',
-			Name: 'Sweet Item',
-			Price: 5
-		},
-		2: {
-			Photo: './img/sweets-2.jpeg',
-			Name: 'Sweet Item',
-			Price: 10
-		},
-		3: {
-			Photo: './img/sweets-3.jpeg',
-			Name: 'Sweet Item',
-			Price: 15
-		}
-	},
-	'Doughnuts': {
-		1: {
-			Photo: './img/doughnut-1.jpeg',
-			Name: 'Dougnut Item',
-			Price: 5
-		},
-		2: {
-			Photo: './img/doughnut-2.jpeg',
-			Name: 'Dougnut Item',
-			Price: 10
-		},
-		3: {
-			Photo: './img/doughnut-3.jpeg',
-			Name: 'Dougnut Item',
-			Price: 15
-		}
-	}
-}
-
-var storeNav = document.querySelector('#store-nav').addEventListener('click', function (event) {
-	var id = event.target.id;
-	alert(id);
-	if (id != 'store-nav') {
-		showProducts(getLen(id));
-	} else {
-		return false;
-	}
-})
-
-var getLen = function(id) {
-	var arr = Object.keys(products);
-	var arrProducts = products;
-	console.log(id);
-	var elem = 0;
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] == id) {
-			for (let i = 1; arrProducts[id][i] !== undefined; i++) {
-				elem++;
-			}
-			return elem;
-		} else {
-			continue;
-		}
-	}
-}
-
-function showProducts(len) {
-	
-}
-
-var border = document.querySelector('#border');
+/*var border = document.querySelector('#border');
+var image = document.querySelector('#main-image');
+image.onmousemove = move;
+image.onmouseout = out;
 function move() {
 	border.style.marginLeft = '-10px';
 	border.style.marginTop = '0px';
@@ -109,4 +10,34 @@ function move() {
 function out() {
 	border.style.marginLeft = '-55px';
 	border.style.marginTop = '-55px';
+}
+*/
+var names = document.querySelectorAll('.name-item');
+var prices = document.querySelectorAll('.price-item');
+
+for (let i = 0; i < names.length; i++) {
+	names[i].innerHTML = names[i].dataset.name;
+	prices[i].innerHTML = prices[i].dataset.price;
+}
+
+var btnFilterClick = document.querySelector('#store-nav').addEventListener('click', function (event) {
+	var id = event.target.id;
+	if (id != 'store-nav') {
+		return showItems(id);
+	} else {
+		return false;
+	}
+});
+
+function showItems(id) {
+	var products = document.querySelectorAll('.store-item');
+	for (let i = 0; i < products.length; i++) {
+		if (id == 'All') {
+			products[i].style.display = 'flex';
+		} else if (products[i].dataset.category != id) {
+			products[i].style.display = 'none';
+		} else {
+			products[i].style.display = 'flex';
+		}
+	}
 }
